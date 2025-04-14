440e1427bab525245cbb038e27c302e205cc0b36
ValeriaSudarikova <lerbel@list.ru>
2025-04-14 19:23:20 +0300

    fix: fix errors, closes #1

--- vue-progect/src/components/TodoList.vue
+++ vue-progect/src/components/TodoList.vue
@@ -1,11 +1,90 @@
-<script setup>
-
-</script>
-
 <template>
-    
+    <div class="tasks">
+        <h4 class="tasks-heading">{{ title }} - {{ count }}</h4>
+        <transition-group name="tasks-list" tag="ul" class="tasks-list">
+            <TodoListItem 
+            v-for="todo in tasksList" :key="todo.id"
+            :todo="todo"
+            @taskCompleted="taskCompleted"
+            @removeTodo="removeTodo"/>
+        </transition-group>
+        <transition name="tasks-list">
+            <p v-if="count === 0" class="tasks-message">{{ text }}</p>
+        </transition>
+    </div>
 </template>
 
-<style lang="scss" scoped>
+<script setup>
+import TodoListItem from '@/components/TodoListItem.vue'
 
+defineProps ({
+    title: {
+        type: String,
+        required: true,
+    },
+    count: {
+        type: Number,
+        required: true,
+    },
+    tasksList: {
+        type: Array,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    }
+})
+
+const emit = defineEmits(['taskCompleted', 'removeTodo'])
+
+const taskCompleted = (todo) => {
+    emit('taskCompleted', todo)
+}
+const removeTodo = (todo) => {
+    emit('removeTodo', todo)
+}
+</script>
+
+<style lang="scss">
+    .tasks {
+        margin: 60px 0;
+
+        &-heading {
+            margin-bottom: 16px;
+        }
+
+        &-list {
+            display: flex;
+            flex-direction: column;
+            gap: 16px;
+        }
+
+        &-message {
+            display: flex;
+            justify-content: center;
+            font-size: 14px;
+        }
+    }
+
+    .tasks-list-move, .tasks-list-enter-active, .tasks-list-leave-active {
+        transition: all .9s;
+    }
+
+    .tasks-list-enter-from, .tasks-list-leave-to {
+        opacity: 0;
+        transform: translate(200px);
+    }
+
+    .tasks-done {
+
+        .tasks-list {
+
+            .task-item {
+                color: #78cfb0;
+                margin-bottom: 16px;
+                text-decoration: line-through;
+            }
+        }
+    }
 </style>
---
+++ vue-progect/src/components/TodoListItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <li class="task-item">
+        <span class="task-item-text">{{ todo.title }}</span>
+        <SvgCheckedBacket 
+        :todo="todo"
+        @taskCompleted="taskCompleted"
+        @removeTodo="removeTodo"
+        />
+    </li>
+</template>
+
+<script setup>
+import SvgCheckedBacket from '@/components/SvgCheckedBacket.vue'
+
+defineProps({
+    todo: {
+        type: Object,
+        required: true,
+    }
+})
+
+const emit = defineEmits(['taskCompleted', 'removeTodo'])
+
+const taskCompleted = (todo) => {
+    emit('taskCompleted', todo)
+}
+const removeTodo = (todo) => {
+    emit('removeTodo', todo)
+}
+</script>
+
+<style lang="scss">
+    .task-item {
+        padding: 28px 20px;
+        background-color: #15101C;
+        border-radius: 10px;
+        color: #9e78cf;
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        &-text {
+            word-break: break-all;
+        }
+    }
+</style>
